e1593d32ea59e2f9a452d0f86b2cfbc4fd59823f
Anitakao <anitakao6321@gmail.com>
2023-11-15 03:36:41 +0800

    第六次推入

--- 11144139.js
+++ 11144139.js
@@ -2,3 +2,23 @@ function toggleInterests() {
     var interestsContainer = document.getElementById("interests-container");
     interestsContainer.style.display = (interestsContainer.style.display === "none" || interestsContainer.style.display === "") ? "block" : "none";
 }
+
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      window.scrollTo({
+        top: section.offsetTop,
+        behavior: 'smooth',
+      });
+    }
+  }
+  
+document.addEventListener("DOMContentLoaded", function () {
+    var showInfoText = document.querySelector('.show-info');
+    var infoBox = document.createElement('div');
+    infoBox.className = 'info-box';
+    infoBox.innerHTML = '暑假期間我'; // 添加介紹內容
+  
+    showInfoText.parentNode.appendChild(infoBox);
+  });
+  
