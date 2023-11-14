3cb3a74e940149cc72958d7505167a8495284673
Anitakao <anitakao6321@gmail.com>
2023-11-14 23:35:32 +0800

    第五次推入

---
+++ 11144139.js
@@ -0,0 +1,4 @@
+function toggleInterests() {
+    var interestsContainer = document.getElementById("interests-container");
+    interestsContainer.style.display = (interestsContainer.style.display === "none" || interestsContainer.style.display === "") ? "block" : "none";
+}
