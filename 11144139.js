de9126d635bb54805411d0e7b5983e27828e6269
Anitakao <anitakao6321@gmail.com>
2023-11-15 13:46:22 +0800

    第八次推入

--- 11144139.js
+++ 11144139.js
@@ -17,7 +17,36 @@ document.addEventListener("DOMContentLoaded", function () {
     var showInfoText = document.querySelector('.show-info');
     var infoBox = document.createElement('div');
     infoBox.className = 'info-box';
-    infoBox.innerHTML = '暑假期間我'; // 添加介紹內容
+    infoBox.innerHTML = '在升上大一的暑假期間我去我父母的公司進行實習，'; // 添加介紹內容
   
     showInfoText.parentNode.appendChild(infoBox);
   });
+
+  // script.js
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+window.onscroll = function () {
+    toggleScrollToTopButton();
+};
+
+function toggleScrollToTopButton() {
+    var button = document.getElementById('scrollToTopBtn');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+}
+
+function toggleDescription() {
+    var description = document.getElementById('description');
+    description.style.display = (description.style.display === 'none' || description.style.display === '') ? 'block' : 'none';
+  }
+
+function toggleDescription2() {
+    var description2 = document.getElementById('description2');
+    description2.style.display = (description2.style.display === 'none' || description2.style.display === '') ? 'block' : 'none';
+  }
