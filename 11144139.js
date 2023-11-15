b51146b15d14ab11a8f5e1423ceadad044e6b197
Anitakao <anitakao6321@gmail.com>
2023-11-15 18:26:26 +0800

    第九次推入

--- 11144139.js
+++ 11144139.js
@@ -2,15 +2,6 @@ function toggleInterests() {
     var interestsContainer = document.getElementById("interests-container");
     interestsContainer.style.display = (interestsContainer.style.display === "none" || interestsContainer.style.display === "") ? "block" : "none";
 }
-  
-document.addEventListener("DOMContentLoaded", function () {
-    var showInfoText = document.querySelector('.show-info');
-    var infoBox = document.createElement('div');
-    infoBox.className = 'info-box';
-    infoBox.innerHTML = '在升上大一的暑假期間我去我父母的公司進行實習，'; // 添加介紹內容
-  
-    showInfoText.parentNode.appendChild(infoBox);
-  });
 
   // script.js
 function scrollToTop() {
